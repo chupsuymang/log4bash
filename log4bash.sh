0a23f1dcadf893d335e0c88ed7e82e63a91fe7f5
chupsuymang <to.svenramm@gmail.com>
2018-12-20 21:34:54 +0100

    Update log4bash.sh

--- log4bash.sh
+++ log4bash.sh
@@ -1,141 +1,107 @@
-#!/usr/bin/env bash
-#--------------------------------------------------------------------------------------------------
-# log4bash - Makes logging in Bash scripting suck less
-# Copyright (c) Fred Palmer
-# Licensed under the MIT license
-# http://github.com/fredpalmer/log4bash
-#--------------------------------------------------------------------------------------------------
-set -e  # Fail on first error
+#!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#				
+# Script Name:	log4bash.sh
+#
+# Author:		Sven Ramm
+# Date:			12/19/2018
+# Version:		2.0
+#
+# Fork of:		http://github.com/fredpalmer/log4bash
+# 				Copyright (c) 2009-2011, Fred Palmer and contributors.
+#				
+# Purpose:		- Logging for bash scripts inclusive log rotation
+#
+# Notes:		- Usage:
+#					log "info" "MESSAGE"
+#					log "error" "MESSAGE"
+#					log "warn" "MESSAGE"
+#					log "success" "MESSAGE"
+#					log "debug" "MESSAGE"
+#
+# Changes:		- 12/19/2018
+#                   - Script creation
+#
+# License:		Licensed under the MIT license
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Useful global variables that users may wish to reference
 SCRIPT_ARGS="$@"
-SCRIPT_NAME="$0"
-SCRIPT_NAME="${SCRIPT_NAME#\./}"
-SCRIPT_NAME="${SCRIPT_NAME##/*/}"
+SCRIPTNAME="$0"
+SCRIPTNAME="${SCRIPTNAME##*/}"
+SCRIPTNAME="${SCRIPTNAME#\./}"
 SCRIPT_BASE_DIR="$(cd "$( dirname "$0")" && pwd )"
 
-# This should probably be the right way - didn't have time to experiment though
-# declare -r INTERACTIVE_MODE="$([ tty --silent ] && echo on || echo off)"
-declare -r INTERACTIVE_MODE=$([ "$(uname)" == "Darwin" ] && echo "on" || echo "off")
+declare -r LOG_DEFAULT_COLOR="\033[0m"
+declare -r LOG_ERROR_COLOR="\033[1;31m"
+declare -r LOG_INFO_COLOR="\033[1;37m"
+declare -r LOG_SUCCESS_COLOR="\033[1;32m"
+declare -r LOG_WARN_COLOR="\033[1;33m"
+declare -r LOG_DEBUG_COLOR="\033[1;34m"
 
-#--------------------------------------------------------------------------------------------------
-# Begin Help Section
+# Max filesize for LOG rotation
+declare -r MaxLogSize=2048 # (2MB)
+declare -r LOG_FILE="~/Library/Logs/log4bash.log"
 
-HELP_TEXT=""
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# Function for Logging incl log rotation
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+log (){
+	local log_level="$1"
+	local log_text="$2"
+	
+	# Default to Info if second parameter is found missing assuming only message is provided
+	[[ -z ${log_text} ]] && log_text="$1";
 
-# This function is called in the event of an error.
-# Scripts which source this script may override by defining their own "usage" function
-usage() {
-    echo -e "${HELP_TEXT}";
-    exit 1;
-}
+	shopt -s nocasematch
+	case "${log_level}" in
+	'info')
+		log_level="INFO"
+		log_color=${LOG_INFO_COLOR}
+	;;
+	'error')
+		log_level="ERROR"
+		log_color=${LOG_ERROR_COLOR}
+	;;
+	'warn')
+		log_level="WARNING"
+		log_color=${LOG_WARN_COLOR}
+	;;
+	'success')
+		log_level="SUCCESS"
+		log_color=${LOG_SUCCESS_COLOR}
+	;;
+	'debug')
+		log_level="DEBUG"
+		log_color=${LOG_DEBUG_COLOR}
+	;;
+	*)
+		log_level="INFO"
+		log_color=${LOG_DEFAULT_COLOR}
+	;;
+	esac
 
-# End Help Section
-#--------------------------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------------------------
-# Begin Logging Section
-if [[ "${INTERACTIVE_MODE}" == "off" ]]
-then
-    # Then we don't care about log colors
-    declare -r LOG_DEFAULT_COLOR=""
-    declare -r LOG_ERROR_COLOR=""
-    declare -r LOG_INFO_COLOR=""
-    declare -r LOG_SUCCESS_COLOR=""
-    declare -r LOG_WARN_COLOR=""
-    declare -r LOG_DEBUG_COLOR=""
-else
-    declare -r LOG_DEFAULT_COLOR="\033[0m"
-    declare -r LOG_ERROR_COLOR="\033[1;31m"
-    declare -r LOG_INFO_COLOR="\033[1m"
-    declare -r LOG_SUCCESS_COLOR="\033[1;32m"
-    declare -r LOG_WARN_COLOR="\033[1;33m"
-    declare -r LOG_DEBUG_COLOR="\033[1;34m"
-fi
-
-# This function scrubs the output of any control characters used in colorized output
-# It's designed to be piped through with text that needs scrubbing.  The scrubbed
-# text will come out the other side!
-prepare_log_for_nonterminal() {
-    # Essentially this strips all the control characters for log colors
-    sed "s/[[:cntrl:]]\[[0-9;]*m//g"
-}
-
-log() {
-    local log_text="$1"
-    local log_level="$2"
-    local log_color="$3"
-
-    # Default level to "info"
-    [[ -z ${log_level} ]] && log_level="INFO";
-    [[ -z ${log_color} ]] && log_color="${LOG_INFO_COLOR}";
-
-    echo -e "${log_color}[$(date +"%Y-%m-%d %H:%M:%S %Z")] [${log_level}] ${log_text} ${LOG_DEFAULT_COLOR}";
-    return 0;
-}
-
-log_info()      { log "$@"; }
-
-log_speak()     {
-    if type -P say >/dev/null
-    then
-        local easier_to_say="$1";
-        case "${easier_to_say}" in
-            studionowdev*)
-                easier_to_say="studio now dev ${easier_to_say#studionowdev}";
-                ;;
-            studionow*)
-                easier_to_say="studio now ${easier_to_say#studionow}";
-                ;;
-        esac
-        say "${easier_to_say}";
-    fi
-    return 0;
-}
-
-log_success()   { log "$1" "SUCCESS" "${LOG_SUCCESS_COLOR}"; }
-log_error()     { log "$1" "ERROR" "${LOG_ERROR_COLOR}"; log_speak "$1"; }
-log_warning()   { log "$1" "WARNING" "${LOG_WARN_COLOR}"; }
-log_debug()     { log "$1" "DEBUG" "${LOG_DEBUG_COLOR}"; }
-log_captains()  {
-    if type -P figlet >/dev/null;
-    then
-        figlet -f computer -w 120 "$1";
-    else
-        log "$1";
+	echo -e "${log_color}[$(date +"%Y-%m-%d %H:%M:%S %Z")] [${log_level}] ${log_text} ${LOG_DEFAULT_COLOR}";
+	
+	# Log into File if given and considered Log rotation
+	if [[ ! -z "${LOG_FILE}" ]]; then
+		touch ${LOG_FILE}
+		
+		# Get size in kb** 
+		file_size=`du -k "${LOG_FILE}" | cut -f1`
+		if [ $file_size -gt ${MaxLogSize} ];then   
+			timestamp=`date +"%Y-%m-%d_%H:%M:%S_%Z"`
+			mv ${LOG_FILE} ${LOG_FILE}.${timestamp}
+			touch ${LOG_FILE}
+		fi
+	
+    	echo -e "[$(date +"%Y-%m-%d %H:%M:%S %Z")] [${SCRIPTNAME}] [${log_level}] ${log_text} " >> ${LOG_FILE}
     fi
     
-    log_speak "$1";
-
     return 0;
 }
 
-log_campfire() {
-    # This function performs a campfire notification with the arguments passed to it
-    if [[ -z ${CAMPFIRE_API_AUTH_TOKEN} || -z ${CAMPFIRE_NOTIFICATION_URL} ]]
-    then
-        log_warning "CAMPFIRE_API_AUTH_TOKEN and CAMPFIRE_NOTIFICATION_URL must be set in order log to campfire."
-        return 1;
-    fi
 
-    local campfire_message="
-    {
-        \"message\": {
-            \"type\":\"TextMessage\",
-            \"body\":\"$@\"
-        }
-    }"
-
-    curl                                                            \
-        --write-out "\r\n"                                          \
-        --user ${CAMPFIRE_API_AUTH_TOKEN}:X                         \
-        --header 'Content-Type: application/json'                   \
-        --data "${campfire_message}"                                \
-        ${CAMPFIRE_NOTIFICATION_URL}
-
-    return $?;
-}
-
-# End Logging Section
-#--------------------------------------------------------------------------------------------------
-
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
